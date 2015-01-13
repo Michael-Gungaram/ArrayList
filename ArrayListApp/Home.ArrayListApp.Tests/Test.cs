06f9337e21b084f0d1435ade1f57acc319167154
Mika <michael.gungaram@gmail.com>
2015-01-13 08:49:24 +0100

    Initial commit. Array List of int, iterations 1, 2, 3, 4, 5.

---
+++ ArrayListApp/Home.ArrayListApp.Tests/Test.cs
@@ -0,0 +1,223 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Home.ArrayListApp.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void create_a_list_with_the_right_capacity()
+        {
+            ArrayListInt array = new ArrayListInt( 5 );
+            int sizeOf = array.Count;
+            Assert.That(sizeOf, Is.EqualTo( 5 ) );
+        }
+
+        // Unit Test on iteration 1
+
+        //[Test]
+        //public void add_an_element_above_the_capacity_throws_ArgumentOutOfRangeException()
+        //{
+        //    ArrayListInt myArr = new ArrayListInt(2);
+        //    myArr.Add(1);
+        //    myArr.Add(2);
+        //    Assert.Throws<ArgumentOutOfRangeException>(() => { myArr.Add(3); });
+        //}
+
+        [Test]
+        public void is_the_right_value()
+        {
+            ArrayListInt myArr = new ArrayListInt(3);
+            myArr.Add(2);
+            int value = myArr.GetItem(0);
+            Assert.That(value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void setItem_is_working_well()
+        {
+            ArrayListInt myArr = new ArrayListInt(3);
+            myArr.Add(12);
+            int value = myArr.GetItem( 0 );
+            Assert.That( value, Is.EqualTo( 12 ) );
+
+            myArr.SetItem(0, 15);
+            int newValue = myArr.GetItem( 0 );
+            Assert.That(newValue, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void set_an_item_at_a_negative_index_or_above_the_arrayList_capacity_throws_ArgumentOutOfRangeException()
+        {
+            ArrayListInt myArray = new ArrayListInt(3);
+            Assert.Throws<ArgumentOutOfRangeException>( () => { myArray.SetItem(58, 23); } );
+            Assert.Throws<ArgumentOutOfRangeException>( () => { myArray.SetItem(-255, 25); } );
+        }        
+
+        [Test]
+        public void get_an_item_at_a_negative_index_or_above_the_arrayList_capacity_throws_ArgumentOutofRangeException()
+        {
+            int value = 0;
+            ArrayListInt array = new ArrayListInt(3);
+            array.Add(22);
+            array.Add(67);
+            array.Add(45);
+            Assert.Throws<ArgumentOutOfRangeException>( () => { value = array.GetItem( 3 ); } );
+        }
+
+        [Test]
+        public void insert_at_an_index_and_get_the_right_element()
+        {
+            int value = 0;
+            int value2 = 0;
+            int value3 = 0;
+            int value4 = 0;
+
+            ArrayListInt array = new ArrayListInt(4);
+
+            array.Add(5);
+            array.Add(10);
+            array.Add(15);
+            array.Add(20);
+            array.Insert(0, 10);
+
+            value = array.GetItem(0);
+            value2 = array.GetItem(1);
+            value3 = array.GetItem(2);
+            value4 = array.GetItem(3);
+
+            Assert.That(value == 10);
+            Assert.That(value2 == 10);
+            Assert.That(value3 == 15);
+            Assert.That(value4 == 20);
+        }
+
+        // Unit Test on iteration 2
+        //[Test]
+        //public void insert_at_over_arrayList_capacity_throws_ArgumentOutOfRangeException()
+        //{
+        //    ArrayListInt array = new ArrayListInt(4);
+
+        //    array.Add(5);
+        //    array.Add(10);
+        //    array.Add(15);
+        //    array.Add(20);
+
+        //    Assert.Throws<ArgumentOutOfRangeException> ( () => array.Insert(5, 10) );
+        //    Assert.Throws<ArgumentOutOfRangeException>(() => array.Insert(-1, 10));
+        //}
+        
+        [Test]
+        public void well_removed()
+        {
+            ArrayListInt array = new ArrayListInt(4);
+            int value = 0;
+            int value2 = 0;
+            int value3 = 0;
+            int value4 = 0;
+
+            array.Add(5);
+            array.Add(10);
+            array.Add(15);
+            array.Add(20);
+
+            array.RemoveAt(1);
+
+            value = array.Count;
+            value2 = array.GetItem(0);
+            value3 = array.GetItem(1);
+            value4 = array.GetItem(2);
+
+            Assert.That( value == 3 );
+            Assert.That( value2 == 5 );
+            Assert.That(value3 == 15);
+            Assert.That(value4 == 20);
+        }
+
+        [Test]
+        public void remove_out_capabilites_of_arrayList_throws_ArgumentException()
+        {
+            ArrayListInt array = new ArrayListInt(4);
+
+            array.Add(5);
+            array.Add(10);
+            array.Add(15);
+            array.Add(20);
+
+            Assert.Throws<ArgumentException>( () => array.RemoveAt( 5 ) );
+            Assert.Throws<ArgumentException>( () => array.RemoveAt( -8 ) );
+        }
+
+        [Test]
+        public void clear_the_arrayList_correctly()
+        {
+            ArrayListInt array = new ArrayListInt(4);
+
+            array.Add(5);
+            array.Add(10);
+            array.Add(15);
+            array.Add(20);
+
+            array.Clear();
+
+            int value = array.Count;
+            int index = array.GetIndex;
+            array.Add(3712);
+            int number = array.GetItem(0);
+
+            Assert.That(value, Is.EqualTo(4));
+            Assert.That(index, Is.EqualTo(0));
+            Assert.That(number == 3712);
+        }
+
+        [Test]
+        public void now_can_add_an_element_even_the_arrayList_is_full()
+        {
+            ArrayListInt array = new ArrayListInt(3);
+
+            array.Add(5);
+            array.Add(10);
+            array.Add(15);
+            array.Add(20);
+
+            int value = array.Count;
+            int number = array.GetItem(3);
+
+            Assert.That(value == 6);
+            Assert.That( number, Is.EqualTo(20) );
+        }
+
+        [Test]
+        public void now_can_insert_into_the_arrayList_even_its_full()
+        {
+            ArrayListInt array = new ArrayListInt(3);
+            array.Add(10);
+            array.Add(20);
+            array.Add(30);
+
+            array.Insert(1, 21);
+            int size = array.Count;
+            int number = array.GetItem(1);
+
+            Assert.That(size, Is.EqualTo( 6 ));
+            Assert.That(number == 21);
+        }
+
+        [Test]
+        public void test_of_the_indexer_implemented()
+        {
+            ArrayListInt array = new ArrayListInt(3);
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            int value = array[1];
+            array[2] = 6;
+            int changedValue = array[2];
+
+            Assert.That( value == 2 );
+            Assert.That( changedValue == 6);
+        }
+    }
+}
---
+++ ArrayListApp/Home.ArrayListApp/ArrayListInt.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Home.ArrayListApp
+{
+    public class ArrayListInt
+    {
+        // Private fields
+
+        int _counter;
+        int _arrayCapacity;
+        int[] _myArray;
+
+        public ArrayListInt(int ArrayCapacity)
+        {
+            _arrayCapacity = ArrayCapacity;
+            _myArray = new int[_arrayCapacity];
+            _counter = 0;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return _myArray[index];
+            }
+            set
+            {
+                _myArray[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns the maximum capacity of an array.
+        /// </summary>
+        public int Count
+        {
+            get { return _arrayCapacity; }
+        }
+
+        /// <summary>
+        /// Get the index xxx of an array.
+        /// </summary>
+        public int GetIndex
+        {
+            get { return _counter; }
+        }
+
+        /// <summary>
+        /// Tests if the array is full.
+        /// </summary>
+        public bool IsFull
+        {
+            get { return ( _counter >= _arrayCapacity ); }
+        }
+
+        /// <summary>
+        /// Adds an element to an array.
+        /// </summary>
+        /// <param name="value">What we want to add to.</param>
+        public void Add(int value)
+        {
+            // Iteration 1 :
+
+            //if (IsFull)
+            //    throw new ArgumentOutOfRangeException("Array cannot contain another argument because it's full.");
+            //_myArray[_counter++] = value;
+
+            if( IsFull )
+            {
+                Array newArray = Array.CreateInstance( typeof(Int32), _arrayCapacity * 2 );
+                for (int i = 0; i < _myArray.Length; i++ )
+                {
+                    newArray.SetValue(_myArray[i], i);
+                }
+                newArray.SetValue(value, _counter++);
+                _myArray = (int[])newArray;
+                _arrayCapacity = _myArray.Length;
+            }
+            else
+            {
+                _myArray[_counter++] = value;
+            }
+        }
+
+        /// <summary>
+        /// Adds an element to a specified index.
+        /// </summary>
+        /// <param name="index">The index where we want to add to.</param>
+        /// <param name="value">The value that we want to add.</param>
+        public void Insert(int index, int value)
+        {
+            // Iteration 2 : 
+
+            //int[] dump = new int[_arrayCapacity];
+            //if( index < 0 || index > _arrayCapacity-1) 
+            //    throw new ArgumentOutOfRangeException("Array cannot contain another argument because it's full or there's a problem with index asked.");
+            //dump = _myArray;
+            //_myArray[index] = value;
+            //for ( int i = index+1; i < dump.Length; i++ )
+            //{
+            //    _myArray[i] = dump[i];
+            //}
+
+            //dump = null;
+
+            if ( index < 0  )
+                throw new ArgumentOutOfRangeException("Array cannot contain another argument because it's full or there's a problem with index asked.");
+            if( IsFull )
+            {
+                int i = 0;
+                Array newArray = Array.CreateInstance(typeof(Int32), _arrayCapacity * 2);
+                for (i = 0; i < _myArray.Length; i++ )
+                {
+                    newArray.SetValue( _myArray[i], i );
+                }
+                int[] dump = (int[])newArray;
+                _myArray = dump;
+                _myArray[index] = value;
+                _arrayCapacity = _myArray.Length;
+                for (i = index + 1; i < dump.Length; i++ )
+                {
+                    _myArray[i] = dump[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes an element at a specified index.
+        /// </summary>
+        /// <param name="index">The index where we want to remove a value.</param>
+        public void RemoveAt(int index)
+        {
+            int[] dump = new int[_arrayCapacity];
+            if( index < 0 || index >= _arrayCapacity )
+                throw new ArgumentException("There's a problem with the index specified.");
+            dump = _myArray;
+            for (int i = index; i < dump.Length; i++ )
+            {
+                if( i < dump.Length-1 )
+                {
+                    _myArray[i] = dump[i+1];
+                }
+            }
+            _arrayCapacity -= 1;
+            dump = null;
+        }
+
+        /// <summary>
+        /// Get the value at an index specified.
+        /// </summary>
+        /// <param name="index">The index where we want the value.</param>
+        /// <returns></returns>
+        public int GetItem(int index)
+        {
+            if (index >= _arrayCapacity || index < 0)
+                throw new ArgumentOutOfRangeException("There's a problem with the index asked to get.", "index");
+            return _myArray[index];
+        }
+
+        /// <summary>
+        /// Sets an item as a value at a specified index.
+        /// </summary>
+        /// <param name="index">The index where we want to set the value.</param>
+        /// <param name="value">The new value which will be setted.</param>
+        public void SetItem(int index, int value)
+        {
+            if ( index >= _arrayCapacity || index < 0 )
+                throw new ArgumentOutOfRangeException( "There's a problem with the index asked to set.", "index" );
+            _myArray[index] = value;
+        }
+
+        /// <summary>
+        /// Clear the array.
+        /// </summary>
+        public void Clear()
+        {
+            for ( int i = 0; i < _myArray.Length; i++ )
+            {
+                _myArray[i] = 0;
+            }
+            _counter = 0;
+        }
+    }
+}
